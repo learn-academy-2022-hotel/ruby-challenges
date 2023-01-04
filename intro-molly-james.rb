ba934670893e7a2750b3f8190e5d0db9b6525576
James Quillen <james.quillen@gmail.com>
2023-01-04 15:18:23 -0800

    Completed challenge

---
+++ intro-molly-james.rb
@@ -0,0 +1,35 @@
+word = 'happy'
+
+p "I feel #{word} today."
+
+another_word = 'relieved'
+
+p another_word.upcase
+
+p another_word.reverse
+
+p another_word.include?('l')
+
+p another_word.capitalize
+
+p another_word.delete('r')
+
+p another_word.index('e')
+
+p another_word.swapcase
+
+array = ['This Old House', 'Breaking Bad', 'The Office', 'West Wing', 'Parks and Rec']
+
+p array.length
+
+p array.first
+
+p array[3]
+
+array = array.reverse
+
+p array
+
+new_array = []
+
+p new_array << 'The Wire' << 'The Office'
