b445e3ceaa9dcf8b5e7d6a32af6b82da7b5abe66
TorinJacobs <potours@yahoo.com>
2023-01-06 13:01:15 -0800

    began color palette challenge

--- classes-dave-torin.rb
+++ classes-dave-torin.rb
@@ -77,56 +77,89 @@
 
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 
-class Task
-    attr_accessor :title, :complete
+# class Task
+#     attr_accessor :title, :complete
 
-    def initialize(title)
-        @title = title
-        @complete = false
-    end
-    def set_title(title)
-        @title = title
-    end
-    def get_title
-        @title
-    end
-    def task_info
-        if @complete
-            "#{@title} has been completed"
-        else
-            "#{@title} has not been completed"
-        end
-    end
-end
+#     def initialize(title)
+#         @title = title
+#         @complete = false
+#     end
+#     def set_title(title)
+#         @title = title
+#     end
+#     def get_title
+#         @title
+#     end
+#     def task_info
+#         if @complete
+#             "#{@title} has been completed"
+#         else
+#             "#{@title} has not been completed"
+#         end
+#     end
+# end
 
-laundry = Task.new('Laundry')
-p laundry.task_info
-homework = Task.new('Homework')
-p homework.task_info
-dishes = Task.new('Dishes')
-p dishes.task_info
+# laundry = Task.new('Laundry')
+# p laundry.task_info
+# homework = Task.new('Homework')
+# p homework.task_info
+# dishes = Task.new('Dishes')
+# p dishes.task_info
 # output:    # "Laundry has not been completed"   
              # "Homework has not been completed"
              # "Dishes has not been completed"
 
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
-laundry.complete = true
-p laundry.task_info
-p homework.task_info
-homework.complete = true
-p dishes.task_info
-dishes.complete = true
+# laundry.complete = true
+# # p laundry.task_info
+# # p homework.task_info
+# homework.complete = true
+# # p dishes.task_info
+# dishes.complete = true
 # output:   # "Laundry has been completed" 
             # "Homework has been completed" 
             # "Dishes has been completed"
 
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
 # As a developer, I can create a class called ColorPalette.
+
+class ColorPalette
+    attr_accessor :color1, :color2, :color3
+
+    def initialize(color1, color2, color3)
+        @color1 = color1
+        @color2 = color2
+        @color3 = color3
+    end
+end
+
 # As a developer, I can create three instances (objects) of class ColorPalette.
 # e.g green = ColorPalette.new
+
+# green = ColorPalette.new
+# p green
+# #output: #<ColorPalette:0x00000001488cd720>
+# blue = ColorPalette.new
+# p blue
+# #<ColorPalette:0x00000001488cd5b8>
+# red = ColorPalette.new
+# p red
+#<ColorPalette:0x00000001488cd4f0>
+
 # As a developer, I can initialize each instance of the class ColorPalette with three colors.
 # e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+p green
+#output: #<ColorPalette:0x000000014d038970 @color1="Chartreuse", @color2="Kelly", @color3="Seafoam">
+blue = ColorPalette.new('Indigo', 'Lapis', 'Azure')
+p blue
+#output: #<ColorPalette:0x000000010700da40 @color1="Indigo", @color2="Lapis", @color3="Azure">
+red = ColorPalette.new('Cherry', 'Rose', 'Blood')
+p red
+#output: #<ColorPalette:0x000000010700d7c0 @color1="Cherry", @color2="Rose", @color3="Blood">
+
+
 # As a developer, I can print the value of each individual color.
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
 # As a developer, I can change one or more colors of a given palette.
