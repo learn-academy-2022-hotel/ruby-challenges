ed7df90a2f821ad07d4da98be9dbfffd388a752a
oagomezz <ogomezbarragan437@gmail.com>
2023-01-06 12:02:09 -0800

    completed colorpallete  challenges

--- classes-oscar-octavio.rb
+++ classes-oscar-octavio.rb
@@ -6,39 +6,38 @@
 # As a developer, I can see the title of each instance of class Task.
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
-class Task
-    def initialize(title)
-        @title = title
-        @status = "incomplete"
-    end
+# class Task
+#     def initialize(title)
+#         @title = title
+#         @status = "incomplete"
+#     end
 
-    def get_title
-        @title
-    end
+#     def get_title
+#         @title
+#     end
 
-    def set_status
-        if @status == "incomplete"
-            @status = "complete"
-        else 
-            @status = "incomplete"
-        end
-    end
-end
+#     def set_status
+#         if @status == "incomplete"
+#             @status = "complete"
+#         else 
+#             @status = "incomplete"
+#         end
+#     end
+# end
 
-laundry = Task.new('laundry')
-p laundry.get_title
-p laundry
-laundry.set_status
-p laundry
-laundry.set_status
-p laundry
+# laundry = Task.new('laundry')
+# p laundry.get_title
+# p laundry
+# laundry.set_status
+# p laundry
+# laundry.set_status
+# p laundry
 
-wash_dishes = Task.new('wash dishes')
-rake_leaves = Task.new('rake leaves')
+# wash_dishes = Task.new('wash dishes')
+# rake_leaves = Task.new('rake leaves')
 
 
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
-
 # As a developer, I can create a class called ColorPalette.
 # As a developer, I can create three instances (objects) of class ColorPalette.
 # e.g green = ColorPalette.new
@@ -47,3 +46,33 @@ rake_leaves = Task.new('rake leaves')
 # As a developer, I can print the value of each individual color.
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
 # As a developer, I can change one or more colors of a given palette.
+class ColorPalette 
+    attr_writer :color1,:color2,:color3?
+
+    def initialize(color1, color2, color3)
+        @color1 = color1
+        @color2 = color2
+        @color3 = color3
+    end
+
+    # def get_color
+    #     [@color1,
+    #     @color2,
+    #     @color3]
+    # end     
+    def all_colors
+      p  "The first color is #{color1}, the second color is #{color2} and the last color is #{color3}."
+    end        
+end
+
+green_colors= ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+# red = ColorPalette.new('red')
+# blue = ColorPalette.new('blue')
+# p green_colors.color1
+# p red.color
+# p blue.color
+# green_colors.all_colors
+p green_colors.color1
+green_colors.color1 = 'red'
+p green_colors.color1
+# p green_colors.get_color
